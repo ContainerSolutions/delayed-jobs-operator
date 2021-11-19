411217478ea091e02a98265c4f1c46aa40b9cc18
Christiaan Vermeulen <chrism.vermeulen@gmail.com>
2021-11-19 17:49:17 +0100

    Give Operator permissions to administrate Jobs

--- controllers/delayedjob_controller.go
+++ controllers/delayedjob_controller.go
@@ -37,6 +37,7 @@ type DelayedJobReconciler struct {
 //+kubebuilder:rbac:groups=batch.container-solutions.com,resources=delayedjobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=batch.container-solutions.com,resources=delayedjobs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=batch.container-solutions.com,resources=delayedjobs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
